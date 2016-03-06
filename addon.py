f5835c69ee2062e7f051320485bf995d183e8296
anilbajaj78 <bajajanil@gmail.com>
2016-03-07 00:47:56 +0530

    Ver 0.1
    Removed commented Code

--- addon.py
+++ addon.py
@@ -6,30 +6,16 @@ import json
 import xbmcplugin
 from resources.lib import helpers as h
 
+def main_branch():
+    h.add_dir(addon_handle, base_url, 'Zee Marathi', "http://www.ozee.com/shows/all/zeemarathi", 'Channel')
+    h.add_dir(addon_handle, base_url, 'Zee TV', "http://www.ozee.com/shows/all/zeetv", 'Channel')
+    h.add_dir(addon_handle, base_url, '& TV', "http://www.ozee.com/shows/all/andtv", 'Channel')
+    h.add_dir(addon_handle, base_url, 'Zindagi', "http://www.ozee.com/shows/all/zindagi", 'Channel')
 
-def main_index():
-    #h.add_dir(addon_handle, base_url, 'Current Shows', SHOWS_URL, 'CurrentShows')
-    #h.add_dir(addon_handle, base_url, 'Archive Shows', SHOWS_URL, 'ArchiveShows')
-    h.add_dir(addon_handle, base_url, 'Zee Marathi', "http://www.ozee.com/shows/all/zeemarathi", 'CurrentShows')
-    h.add_dir(addon_handle, base_url, 'Zee TV', "http://www.ozee.com/shows/all/zeetv", 'CurrentShows')
-    h.add_dir(addon_handle, base_url, '& TV', "http://www.ozee.com/shows/all/andtv", 'CurrentShows')
-    h.add_dir(addon_handle, base_url, 'Zindagi', "http://www.ozee.com/shows/all/zindagi", 'CurrentShows')
-    h.add_dir(addon_handle, base_url, 'Movies', "http://www.ozee.com/shows/all/Movies", 'CurrentShows')
-
-def current_shows():
+def shows_serials():
     url = h.extract_var(args, 'url')
 
     soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
-
-    # XXX: If want sorted
-    # import operator
-    # shows = {}
-    # shows[a_attrs['href']] = a_attrs['title']
-    # shows = sorted(shows.items(), key=operator.itemgetter(1))
-
-    # XXX: View mode thumbnail supported in xbmcswift2
-
-    #h2 = soup.findAll('h2')
     
     list = soup.findAll("div", {"class":"thumbnail-with-border-small-title clearfix"})
 
@@ -37,56 +23,20 @@ def current_shows():
         xbmc.log(div.find('a')["href"])
         title = div.find('div').find('span')["title"]
         img_src = div.find('img')["src"]
-        h.add_dir(addon_handle, base_url, title, div.find('a')["href"] + "/video", 'show', img_src, img_src)
+        h.add_dir(addon_handle, base_url, title, div.find('a')["href"] + "/video", 'episode', img_src, img_src)
 
 
-def archive_shows():
+def episode():
     url = h.extract_var(args, 'url')
-
-    soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
-
-    h2 = soup.find_all('h2')
-
-    for h2 in soup.find_all('h2'):
-        if h2.text == 'Archive Shows':
-            for div in h.bs_find_all_with_class(h2.nextSibling, 'div', 'archive-show'):
-                a = div.find('a')
-                a_attrs = dict(a.attrs)
-                h.add_dir(addon_handle, base_url, a_attrs['title'], '%s/video/' % a_attrs['href'], 'show')
-            break
-
-
-def show():
-    url = h.extract_var(args, 'url')
-
-    xbmc.log("URL :" + url)
-    #url = '%s%s' % (ZEEMARATHI_REFERRER, url)
 
     soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
 
     list = soup.findAll("div", {"class":"col-md-3 col-xs-6 reduce-padding"})
-    #nCnt = 0
 
     for div in list:
-        #if nCnt == 8:
-        #    break
-        #nCnt = nCnt + 1
-
-        #xbmc.log('HREF : " + div.find('a')["href"])
         episode_url = div.find('a')["href"]
         img_src = div.find('img')["src"]
-        h.add_dir(addon_handle, base_url, div.find('img')['title'], episode_url, 'episode', img_src, img_src)
-
-#    ul = soup.find('ul', {'class': lambda x: x and 'show-videos-list' in x.split()})
-#    for li in ul:
-#        div = li.find('div', {'class': lambda x: x and 'video-watch' in x.split()})
-#        episode_url = div.find('a')['href']
-#        name = li.find('div', {'class': 'video-episode'}).text
-#        img_src = 'DefaultFolder.png'
-#        img = li.find('img')
-#        if img:
-#            img_src = img['src']
-
+        h.add_dir(addon_handle, base_url, div.find('img')['title'], episode_url, 'show', img_src, img_src)
 
     pager = soup.find('ul', {'class': lambda x: x and 'pager' in x.split()})
     if pager:
@@ -97,12 +47,10 @@ def show():
                 h.add_dir(addon_handle, base_url, 'Next >>', next_url, 'show')
 
 
-def episode():
+def show():
     url = h.extract_var(args, 'url')
 
     name = h.extract_var(args, 'name')
-
-    #xbmc.log("URL : " + url)
 	
     soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
     
@@ -117,36 +65,28 @@ def episode():
             break
 		
     master_m3u8 = script.text.split('playbackurl = ')[1].split('\";')[0][1:]
-    xbmc.log("Master : " + master_m3u8)
-    #plot = soup.find('p', {'itemprop': 'description'}).text
-    #xbmc.log("JSON : " + soup.find('script', {'type': 'application/ld+json'}).text)
+
     data = json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
 	
     thumbnail = data["video"]["thumbnailUrl"]
     plot = data["video"]["description"]
-	#name = data["video"]["name"]
     h.add_dir_video(addon_handle, name, master_m3u8, thumbnail, plot)
 
 
 def not_implemented():
     pass
 
-ZEEMARATHI_REFERRER = 'http://www.ozee.com'
-SHOWS_URL = '%s/shows/' % ZEEMARATHI_REFERRER
-
 addon_id = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 cookie_file, cookie_jar = h.init_cookie_jar(addon_id)
 
 base_url = sys.argv[0]
-xbmc.log("Base : " + base_url)
+
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 mode = args.get('mode', ['', ])[0]
-xbmc.log("Mode : " + mode)
-if mode == 'CurrentShows':
-    current_shows()
-elif mode == 'ArchiveShows':
-    archive_shows()
+
+if mode == 'Channel':
+    shows_serials()
 elif mode == 'show':
     show()
 elif mode == 'episode':
@@ -154,6 +94,6 @@ elif mode == 'episode':
 elif mode == 'not_implemented':
     current_shows()
 else:
-    main_index()
+    main_branch()
 
 xbmcplugin.endOfDirectory(addon_handle)
