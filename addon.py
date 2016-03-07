bb862e0eb1cb3c3480b8eec60ee98480f536de5f
anilbajaj78 <bajajanil@gmail.com>
2016-03-07 22:50:52 +0530

    Added Movies & Next button

--- addon.py
+++ addon.py
@@ -11,6 +11,7 @@ def main_branch():
     h.add_dir(addon_handle, base_url, 'Zee TV', "http://www.ozee.com/shows/all/zeetv", 'Channel')
     h.add_dir(addon_handle, base_url, '& TV', "http://www.ozee.com/shows/all/andtv", 'Channel')
     h.add_dir(addon_handle, base_url, 'Zindagi', "http://www.ozee.com/shows/all/zindagi", 'Channel')
+    h.add_dir(addon_handle, base_url, 'Movies', "http://www.ozee.com/movies/all", 'Movies')
 
 def shows_serials():
     url = h.extract_var(args, 'url')
@@ -25,6 +26,48 @@ def shows_serials():
         img_src = div.find('img')["src"]
         h.add_dir(addon_handle, base_url, title, div.find('a')["href"] + "/video", 'episode', img_src, img_src)
 
+    pager = soup.find('ul', {'class': lambda x: x and 'pagination' in x.split()})
+    if pager is not None:
+        strPage = url.split("?page=")
+        if len(strPage) == 2:
+            nextPage = int(strPage[1]) + 1
+        else:
+            nextPage = 2
+        for pg in pager:
+            if (hasattr(pg, "text")) and (pg.text == str(nextPage)):
+                nextUrl = strPage[0] + "?page=" + str(nextPage)
+                h.add_dir(addon_handle, base_url, 'Next >>', nextUrl, 'Movies')
+                break
+        h.add_dir(addon_handle, base_url, '<< Home >>', "", '')
+
+
+def shows_movies():
+    url = h.extract_var(args, 'url')
+    xbmc.log("Movies URL : " + url)
+    soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
+    
+    list = soup.findAll("div", {"class":"thumbnail-with-border-small-title clearfix"})
+
+    for div in list:
+        title = div.find('div').find('span')["title"]
+        img_src = div.find('img')["src"]
+        h.add_dir(addon_handle, base_url, title, div.find('a')["href"], 'play', img_src, img_src)
+
+    pager = soup.find('ul', {'class': lambda x: x and 'pagination' in x.split()})
+    
+    if pager is not None:
+        strPage = url.split("?page=")
+        if len(strPage) == 2:
+            nextPage = int(strPage[1]) + 1
+        else:
+            nextPage = 2
+        for pg in pager:
+            if (hasattr(pg, "text")) and (pg.text == str(nextPage)):
+                nextUrl = strPage[0] + "?page=" + str(nextPage)
+                h.add_dir(addon_handle, base_url, 'Next >>', nextUrl, 'Movies')
+                break
+        h.add_dir(addon_handle, base_url, '<< Home >>', "", '')
+
 
 def episode():
     url = h.extract_var(args, 'url')
@@ -37,19 +80,51 @@ def episode():
         episode_url = div.find('a')["href"]
         img_src = div.find('img')["src"]
         h.add_dir(addon_handle, base_url, div.find('img')['title'], episode_url, 'show', img_src, img_src)
+'''
+    pager = soup.find('ul', {'class': lambda x: x and 'pagination' in x.split()})
+    
+    if pager is not None:
+        strPage = url.split("?page=")
+        if len(strPage) == 2:
+            nextPage = int(strPage[1]) + 1
+        else:
+            nextPage = 2
+        for pg in pager:
+            if (hasattr(pg, "text")) and (pg.text == str(nextPage)):
+                nextUrl = strPage[0] + "?page=" + str(nextPage)
+                h.add_dir(addon_handle, base_url, 'Next >>', nextUrl, 'Movies')
+                break
+        h.add_dir(addon_handle, base_url, '<< Home >>', "", '')
+'''
+def play_movie():
+    url = h.extract_var(args, 'url')
 
-    pager = soup.find('ul', {'class': lambda x: x and 'pager' in x.split()})
-    if pager:
-        next_link = pager.find('li', {'class': lambda x: x and 'pager-next' in x.split()})
-        if next_link:
-            next_url = next_link.find('a')['href']
-            if next_url:
-                h.add_dir(addon_handle, base_url, 'Next >>', next_url, 'show')
+    xbmc.log("URL : " + url)
+    name = h.extract_var(args, 'name')
+	
+    soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
+    
+    div = soup.find("div", {"id":"movie-container"})
 
+    script = None
+    scripts = div.findAll("script")
+
+    for s in scripts:
+        if s.text.find('playbackurl = ') != -1:
+            script = s
+            break
+		
+    master_m3u8 = script.text.split('playbackurl = ')[1].split('\";')[0][1:]
+
+    data = json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
+    thumbnail = data["image"]
+    plot = data["description"]
+    h.add_dir_video(addon_handle, name, master_m3u8, thumbnail, plot)
 
 def show():
     url = h.extract_var(args, 'url')
 
+    xbmc.log("URL : " + url)
     name = h.extract_var(args, 'name')
 	
     soup = BeautifulSoup(h.make_request(url, cookie_file, cookie_jar))
@@ -87,8 +162,12 @@ mode = args.get('mode', ['', ])[0]
 
 if mode == 'Channel':
     shows_serials()
+elif mode == 'Movies':
+    shows_movies()
 elif mode == 'show':
     show()
+elif mode == 'play':
+    play_movie()
 elif mode == 'episode':
     episode()
 elif mode == 'not_implemented':
